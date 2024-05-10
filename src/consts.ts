e6434e412bd414485a0fd42c5aa4280c7244f234
Chathuranga Bandara <chathurangapayment@gmail.com>
2024-05-10 18:49:39 +0530

    added build

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'MeshX';
+export const SITE_DESCRIPTION = 'AI-First Data Platform';
+export const SITE_URL = 'https://atom.tailus.io';
+export const SITE_IMAGE = '/og.png';
