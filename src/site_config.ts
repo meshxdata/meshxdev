4fa6c9602ab3e190c2d562dfc1f813cc8e25d92b
Chathuranga Bandara <156180746+chat-snc@users.noreply.github.com>
2024-05-14 16:22:36 +0530

    updates

--- src/site_config.ts
+++ src/site_config.ts
@@ -1,23 +1,5 @@
-// This file contains the site configuration for the theme.
+export const SITE_TITLE = "MeshX Website"
+export const SITE_DESCRIPTION = "MeshX: Build a powerful AI future on your data"
+export const SITE_URL = "https://www.meshx.dev"
 
-// Metadata, SEO, and Social
-export const SITE_TITLE = "Manual Theme"
-export const SITE_DESCRIPTION = "A documentation template for Astro"
-export const SITE_URL = "https://manual.otterlord.dev"
-export const SITE_DEFAULT_OG_IMAGE = "/assets/og-image.png"
 
-// Docs Sidebar
-// Define the left sidebar items here.
-// The path should match the folder name in src/content/docs/
-export const SIDEBAR_ITEMS = {
-  "Get Started": [
-    "/docs/get-started/introduction/",
-    "/docs/get-started/installation/",
-  ],
-  "Tutorials": [
-    "/docs/tutorials/build-x/",
-  ],
-  "Guides": [
-    "/docs/guides/migrate-from-z/",
-  ]
-}
