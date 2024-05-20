26768454c9f5f806c946c5acf1aeae6c788e3a76
Chathuranga Bandara <156180746+chat-snc@users.noreply.github.com>
2024-05-20 14:38:05 +0530

    added content astro pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,33 @@ module.exports = {
       fontFamily: {
         montserrat: ['"Montserrat"'],
         inter: ['"Inter"'],
+        typography: {
+          DEFAULT: {
+            css: {
+              h1: {
+                fontSize: '2.25rem', // 36px
+                fontWeight: '700',  // bold
+                lineHeight: '2.5rem', // 40px
+                color: '#1a202c', // Tailwind's gray-900
+              },
+              h2: {
+                fontSize: '1.875rem', // 30px
+                fontWeight: '600',  // semi-bold
+                lineHeight: '2.25rem', // 36px
+                color: '#2d3748', // Tailwind's gray-800
+              },
+              h3: {
+                fontSize: '1.5rem', // 24px
+                fontWeight: '600',  // semi-bold
+                lineHeight: '2rem', // 32px
+                color: '#4a5568', // Tailwind's gray-700
+              },
+            },
+          },
+        },
       },
     },
   },
   //   safelist: ['dark:!text-white'],
-  // 	plugins: [require('@tailwindcss/typography')],
+  	plugins: [require('@tailwindcss/typography')],
 };
